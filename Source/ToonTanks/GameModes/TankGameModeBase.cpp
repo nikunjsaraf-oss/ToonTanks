31c7e104b7dcd2c10b8c4091b007534c363c8449
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2021-05-28 22:12:02 +0530

    Add health functionality

--- Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -10,6 +10,7 @@ void ATankGameModeBase::BeginPlay()
 
 void ATankGameModeBase::ActorDied(AActor* DeadActor)
 {
+	UE_LOG(LogTemp, Warning, TEXT("A Pawn Died"));
 }
 
 void ATankGameModeBase::HandleGameStart()
